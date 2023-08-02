0d0f396a2f7642b2d858480ea77058f71618984f
andrei.neagu18 <andreut06@yahoo.com>
2023-08-02 11:48:15 +0300

    Add files via upload

---
+++ src/main/java/andrei/in/restendpointget/RestendpointgetApplication.java
@@ -0,0 +1,13 @@
+package andrei.in.restendpointget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestendpointgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestendpointgetApplication.class, args);
+	}
+
+}
